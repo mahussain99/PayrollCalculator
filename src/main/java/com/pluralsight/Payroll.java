77a4873423cdcdcb9312ba7527ac91e52a4daf22
Family <mh5212357@gmail.com>
2025-10-07 19:54:09 -0400

    I completed this exercise

--- src/main/java/com/pluralsight/Payroll.java
+++ src/main/java/com/pluralsight/Payroll.java
@@ -1,4 +1,59 @@
 package com.pluralsight;
 
-public class PayRoll {
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Payroll {
+    public static void main(String[] args) {
+
+        //Step 1:  Using Scanner for asking user input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the name of employee file name");
+        String employeeFileName = scanner.nextLine();
+        System.out.println(" Enter the payroll file name");
+        String payrollFileName = scanner.nextLine();
+
+        //String fileName = "employees.csv";
+        // BufferedReader to read a text file line-by-line.
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(employeeFileName));
+            BufferedWriter witter = new BufferedWriter(new FileWriter(payrollFileName));
+
+            // "\\|" ---> only use backslash for Pipe
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] tokens = line.split("\\|");
+                int id = Integer.parseInt(tokens[0]);
+                String name = tokens[1];
+                double hoursWorked = Double.parseDouble(tokens[2]);
+                double payRate = Double.parseDouble(tokens[3]);
+
+                // Created employees object and calling from employee class
+
+                Employees employee = new Employees(id, name, hoursWorked, payRate);
+                // payroll out put information are empty now
+                String payrollOutPutLine = "";
+
+                /*System.out.printf("Employee ID: %d, Name: %s, Gross Pay: $%.2f%n",
+                        employee.getEmployeeId(), employee.getName(), employee.getGrossPay());*/
+                // Create a file text file
+                payrollOutPutLine = String.format("%d|%s|$%.2f%n",
+                        employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
+
+                // Store the all data
+                witter.write(payrollFileName);
+            }
+
+            reader.close();
+            witter.close();
+            scanner.close();
+
+
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + employeeFileName);
+            System.out.println("Error reading file : " + payrollFileName);
+        }
+    }
 }
